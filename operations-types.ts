564371a1c30975e6464e04806ef01975ef54b9e3
damonliftz <aidendamon16@gmail.com>
2025-10-31 09:13:04 +0200

    operations-types.ts

---
+++ operations-types.ts
@@ -0,0 +1,15 @@
+let number1 = 42;
+
+let number2 = 17;
+
+let number1String = number1.toString();
+
+let sum = number1 + number2;
+
+let product = number1 * number2;
+
+console.log("number1:", number1);
+console.log("number2:", number2);
+console.log("number1 as string:", number1String);
+console.log("sum:", sum);
+console.log("product:", product);
